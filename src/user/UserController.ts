5bd0f06707c411b932e6eac70688fdcf1d2d9d17
damian <damiangalvan66@gmail.com>
2022-03-11 10:58:21 -0600

    minor changes

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import userService from './UserService';
 
-// @route POST
+// @route POST /api/1.0/users
 export const signIn = async (req: Request, res: Response) => {
   await userService.signIn(req.body);
   res.status(200).send({ message: 'User Created' });
